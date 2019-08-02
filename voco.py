f074d2f288af6ed68196ba8e83bd33051385499f
subaexe47 <53489976+subaexe47@users.noreply.github.com>
2019-08-02 20:08:58 +0530

    Create voco.py

---
+++ voco.py
@@ -0,0 +1,7 @@
+n = input()
+if (n == 'a' or n == 'A' or n == 'e' or n == 'E' or n =='i' or n =='I' or n == 'o' or n == 'O' or n == 'u' or n == 'U'):
+  print('Vowel')
+elif (n != 'a' or n != 'A' or n != 'e' or n != 'E' or n !='i' or n !='I' or n != 'o' or n != 'O' or n != 'u' or n != 'U'):
+  print('Consonant')
+else:
+  print('Invalid')
