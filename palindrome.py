7b1ef4683268a6a7777fd129820b5317e8e4c546
subaexe47 <53489976+subaexe47@users.noreply.github.com>
2019-08-08 15:04:51 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,10 +3,10 @@ reverse=0
 temp=0
 temp=number1
 if (temp>0):
-    temp1=temp1%10
-    reverse=reverse*10+temp1
+    temp=temp%10
+    reverse=reverse*10+temp
     number1=number1//10
-if (temp=reverse):
+if (temp==reverse):
     print("yes")
 else:
     print("no")
